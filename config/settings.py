0eb25c904ef028dd09299d0bd5c9a128d612d904
Nikita <nikita.pymitron@gmail.com>
2024-12-04 16:22:09 +0700

    new installed apps: rest framework and cars

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'cars.apps.CarsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
